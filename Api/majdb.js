9a22ed64110cc250aa97b85bea8dbded66ecfa8d
michael <michael-michaux_student2021@wilder.school>
2022-03-31 12:25:14 +0200

    algo rename lines names

--- Api/majdb.js
+++ Api/majdb.js
@@ -2,7 +2,7 @@ const XLSX = require('xlsx');
 
 
 const readExcel = (file) => {
-    const items = [];
+    let items = [];
 	const xlsFile = XLSX.readFile(file);
 	const tab = xlsFile.SheetNames;
 	const affectation = tab.filter(s => s === 'Affectation_Parc');
@@ -18,15 +18,43 @@ const readExcel = (file) => {
 	}
 }
 
-const rames = readExcel('./Test.xlsx');
-
-console.log(rames);
-
 function jsonToObject(row) {
 	const obj = {};
 	obj.train = row["N° Rame"] ?? 0
 	obj.serie = row["Famille Série"] ?? ""
-	obj.line = row["Axe"] ?? ""
+	obj.line = rename(row["Axe"]) ?? ""
 		
 	return obj;
 }
+
+const rename =(name) => {
+    switch(name) {
+
+        case 'SE_Tsee':
+            return 'Sud-Est'
+            
+        case 'SE_Tlg':
+            return 'Sud-Est'
+            
+        case 'ATL':
+            return 'Atlantique'
+            
+        case 'BHM':
+            return 'Bisheim'
+            
+        case 'FALBALA':
+            return 'FALBALA (Espagne)'
+            
+        case 'FOREST':
+            return 'FOREST (Belgique)'
+            
+        default:
+            return name
+    }
+}
+const rames = readExcel('../Test.xlsx');
+
+console.log(rames)
+
+
+
