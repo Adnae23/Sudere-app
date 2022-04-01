e33ff8e6c791aed0178740687b1481691ffb6102
michael <michael-michaux_student2021@wilder.school>
2022-04-01 14:32:01 +0200

    update db lines & series

--- Api/app.js
+++ Api/app.js
@@ -20,13 +20,58 @@ let trains = [...data[0]]
 let lines = [...data[1]]
 let series = [...data[2]]
 
-console.log(trains)
-console.log(lines)
-console.log(series)
+// console.log(trains)
+// console.log(lines)
+// console.log(series)
 
+// ******************************** ON PARCOUR LES LIGNES DE LINES
 lines.map(line => {
-    let test = connection.query("SELECT * FROM `lines` WHERE name = ?", [line])
-    
-        console.log(test)
-    
+	let existLine = '';
+	const db = connection.promise();
+	const sqlSelect = 'SELECT COUNT(*) AS lineCount FROM `lines` WHERE name = ?'
+	const sqlInsert = 'INSERT INTO `lines` SET name= ?'
+	// *************************** ON VERIFIE SI ELLE EXISTE DEJA LA TABLE
+    db.query(sqlSelect, [line]).then((result) =>{
+		existLine = result[0][0].lineCount;
+		// console.log(result[0][0].lineCount);
+		// *********************** SI ELLE N'EXISTE PAS => ON L'INSERT
+		if(existLine === 0) {
+			return db.query(sqlInsert, [line])
+		}
+		else
+			return Promise.reject('ALREADY_EXIST')
+	})
+	.then(() => {
+		console.log('Insert successful');
+	})
+	.catch(err => {
+		if (err === 'ALREADY_EXIST') 
+			console.log('line already exist');
+	})
+})
+
+// ******************************** ON PARCOUR LES LIGNES DE SERIES
+series.map(serie => {
+	let existSerie = '';
+	const db = connection.promise();
+	const sqlSelect = 'SELECT COUNT(*) AS serieCount FROM series WHERE name = ?'
+	const sqlInsert = 'INSERT INTO series SET name= ?'
+    db.query(sqlSelect, [serie]).then((result) =>{
+	// *************************** ON VERIFIE SI ELLE EXISTE DEJA LA TABLE
+		existSerie = result[0][0].serieCount;
+		// console.log(result[0][0].serieCount);
+		// *********************** SI ELLE N'EXISTE PAS => ON L'INSERT
+		if(existSerie === 0) {
+			return db.query(sqlInsert, [serie])
+		}
+		else
+			return Promise.reject('ALREADY_EXIST')
+	})
+	.then(() => {
+		console.log('Insert successful');
+	})
+	.catch(err => {
+		if (err === 'ALREADY_EXIST') 
+			console.log('Serie already exist');
+	})
 })
--- Api/majdb.js
+++ Api/majdb.js
@@ -1,48 +1,52 @@
 const XLSX = require('xlsx');
 
-
+// *************************** LECTURE DU FICHIER EXCEL
 const readExcel = (file) => {
     let items = [];
 	let data = []
 	const xlsFile = XLSX.readFile(file);
+	// ********************************** ON RECUPERE LA LISTE DES ONGLETS
 	const tab = xlsFile.SheetNames;
+	// ********************************** ON RECHERCHE L'ONGLET "Affectation_Parc"
 	const affectation = tab.filter(s => s === 'Affectation_Parc');
 	let one = affectation.length === 1 ? affectation.join('') : undefined;
+	let lines = []
+	let series = []
+	// ********************************** SI PRESENT => ON RECUPERE LES DONNEES SUR DIFFERENTES COLONNES 
 	if (one) {
 		const excelSheet = xlsFile.Sheets[one];
 		items = XLSX.utils.sheet_to_json(excelSheet);
 		data.push(items.map(row => {
-			return jsonToTrain(row);
-		}))
-		let lines = []
-		let series = []
-		items.map(row => {
+			// ********************* SI L'AXE N'EST PAS PRESENT DANS LE TABLEAU => ON L'AJOUTE
 			if(!lines.includes(rename(row["Axe"])))
 				lines.push(rename(row["Axe"]))
-		})
-		items.map(row => {
+			// ********************* SI LA SERIE N'EST PAS PRESENTE DANS LE TABLEAU => ON L'AJOUTE
 			if(!series.includes(row["Famille Série"]))
 				series.push(row["Famille Série"])
-		})
+			// ********************* ON AJOUTE UN OBJET (rame, axe, serie)
+			return jsonToTrain(row);
+		}))
 		data.push(lines)
 		data.push(series)
 		return data
 	} else {
+		// ************************ SI L'ONGLET N'EXISTE PAS ON LE SIGNALE
 		throw new Error("Il n'existe pas de de table : Affectation_Parc");
 	}
 }
 
+// *************************** CREATION DE L'OBJET
 function jsonToTrain(row) {
 	const train = {};
 	
 	train.id = row["N° Rame"] ?? 0
 	train.serie = row["Famille Série"] ?? ""
-	train.line = rename(row["Axe"]) ?? ""
+	train.line = rename(row["Axe"].replace(/[éèê]/g,"e")) ?? ""
 		
 	return train;
 }
 
-
+// *************************** RENOMME LES AXES
 const rename =(name) => {
     switch(name) {
 
@@ -57,6 +61,9 @@ const rename =(name) => {
             
         case 'BHM':
             return 'Bisheim'
+            
+        case 'Réseau':
+            return 'Reseau'
             
         case 'FALBALA':
             return 'FALBALA (Espagne)'
