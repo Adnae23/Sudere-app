eb10ece3217bf8658a08d9842c5f861dc3a90ce3
Gael D <gaeldouence@gmail.com>
2022-04-14 17:07:44 +0200

    delete excel file after upload

--- Api/db/middlewares/db.middlewares.js
+++ Api/db/middlewares/db.middlewares.js
@@ -1,8 +1,8 @@
 const { getLines, getSeries, getTrains, getCenters } = require('../models/db.models')
-const {compare} = require('../../utils/compare')
+const { compare } = require('../../utils/compare')
 const XLSX = require('xlsx');
 
-class DbMiddlewares{
+class DbMiddlewares {
 
     async storeFile(req, res, next) {
         const file = req.files.excelFile
@@ -14,32 +14,37 @@ class DbMiddlewares{
         res.status(200).send('File uploaded !')
         setTimeout(() => {
             req.xlsFile = XLSX.readFile('./uploadedFiles/Test.xlsm')
+            var fs = require('fs');
+
+            fs.unlink('./uploadedFiles/Test.xlsm', function (err) {
+                if (err) throw err;
+            });
             next()
         }, 3000)
-        
+
     }
 
-    sheetName (req, res, next){
+    sheetName(req, res, next) {
         const tab = req.xlsFile.SheetNames;
-        if (tab.filter(element => element === 'Affectation_Parc')){
-            
+        if (tab.filter(element => element === 'Affectation_Parc')) {
+
             next()
         }
         else
             res.status(404).send('resource not found')
     }
 
-    async compareData(req, res, next){
-        try{
+    async compareData(req, res, next) {
+        try {
             let trainsFromDb = await getTrains()
             compare(req, trainsFromDb)
-            
-            
+
+
         }
         catch (error) {
             res.status(500).send({ error: error.message })
-        } 
-        
+        }
+
         next()
     }
 
