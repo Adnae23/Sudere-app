3d865dd1f83956249a61802d275c070c220412c7
ched <ched5@orange.fr>
2022-05-04 16:28:37 +0200

    install eslint

--- suderereact/src/components/ChoiceHome.jsx
+++ suderereact/src/components/ChoiceHome.jsx
@@ -4,11 +4,15 @@ const ChoiceHome = () => {
     return (
         <div className='content'>
             <div className='content_train'>
-                <img className='content_train_img' src='./pictures/train.jpg' alt='' />
+                <div className='content_train_img'>
+                    <img className='content_train_img_1' src='./pictures/train.jpg' alt='' />
+                </div>
                 <h3 className='content_train_title'>Consultation</h3>
             </div>
             <div className='content_team'>
-                <img className='content_team_img' src='./pictures/team.jpg' alt='' />
+                <div className='content_team_img'>
+                    <img className='content_team_img_2' src='./pictures/team.jpg' alt='' />
+                </div>
                 <h3 className='content_team_title'>Connexion</h3>
             </div>
         </div>
