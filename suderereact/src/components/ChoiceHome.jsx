9d50e7e74b6a6840ce6d0be66d7423fd6b5cc74d
ched <ched5@orange.fr>
2022-05-04 15:45:55 +0200

    change branch name

--- suderereact/src/components/ChoiceHome.jsx
+++ suderereact/src/components/ChoiceHome.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const ChoiceHome = () => {
     return (
-        <div className='global_content'>
+        <div className='content'>
             <div className='content_train'>
-                <img className='content_img' src='./pictures/train.jpg' alt='' />
-                <h3 className='content_title'>Consultation</h3>
+                <img className='content_train_img' src='./pictures/train.jpg' alt='' />
+                <h3 className='content_train_title'>Consultation</h3>
             </div>
             <div className='content_team'>
-                <img className='content_img' src='./pictures/team.jpg' alt='' />
-                <h3 className='content_title'>Connexion</h3>
+                <img className='content_team_img' src='./pictures/team.jpg' alt='' />
+                <h3 className='content_team_title'>Connexion</h3>
             </div>
         </div>
     );
