bfa980a31a8b18955c43625aad2ef3182e4bb6bb
ched <ched5@orange.fr>
2022-05-06 14:47:21 +0200

    modif scss header

--- suderereact/src/components/ChoiceHome.jsx
+++ suderereact/src/components/ChoiceHome.jsx
@@ -4,19 +4,19 @@ import { NavLink } from 'react-router-dom';
 function ChoiceHome() {
   return (
     <div className="content">
-      <button type="button" className="content_train">
-        <div className="content_train_img">
-          <img className="content_train_img_1" src="./pictures/logo/train(2).jpg" alt="train" />
+      <button type="button" className="content__train">
+        <div className="content__train__img">
+          <img className="content__train__img__1" src="./pictures/logo/train(2).jpg" alt="train" />
         </div>
-        <h3 className="content_train_title">Consultation</h3>
+        <h3 className="content__train__title">Consultation</h3>
       </button>
       <NavLink to="/Connexion">
-        <button type="button" className="content_team">
-          <div className="content_team_img">
-            <img className="content_team_img_2" src="./pictures/logo/team.jpg" alt="team" />
+        <button type="button" className="content__team">
+          <div className="content__team__img">
+            <img className="content__team__img__2" src="./pictures/logo/team.jpg" alt="team" />
           </div>
-          <h3 className="content_team_title-1">Connexion</h3>
-          <h3 className="content_team_title-2">(PC et Tablette)</h3>
+          <h3 className="content__team__title-1">Connexion</h3>
+          <h3 className="content__team__title-2">(PC et Tablette)</h3>
         </button>
       </NavLink>
     </div>
