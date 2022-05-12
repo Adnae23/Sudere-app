d3820c52c7826475dbb7c68aa013eb506abbe43b
ched <ched5@orange.fr>
2022-05-12 17:03:19 +0200

    navigate and connect password

--- suderereact/src/components/CommonPage/TrailersModifConnect.jsx
+++ suderereact/src/components/CommonPage/TrailersModifConnect.jsx
@@ -5,7 +5,7 @@ function TrailersModifConnect() {
     <form className="formConnect">
       <div className="formConnect__date">
         <label className="formConnect__date__label" htmlFor="date__intervention">{'Date d\'intervention:'}</label>
-        <input className="formConnect__delai__valeur" type="date" min="2022-05-01" />
+        <input className="formConnect__delai__valeur" type="date" min="2022-01-01" />
       </div>
       <div className="formConnect__delai">
         <label className="formConnect__delai__label" htmlFor="duree__intervention">{'Durée d\'intervention:'}</label>
