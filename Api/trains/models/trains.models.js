72b5982de77697fde774156d21d7664f2925918d
Adnae23 <anais23ard@hotmail.fr>
2022-06-03 11:09:34 +0200

    final commit api

--- Api/trains/models/trains.models.js
+++ Api/trains/models/trains.models.js
@@ -16,7 +16,7 @@ class TrainsModel {
 
   async getTrailers() {
     try {
-      const sql = 'SELECT number, trains.id, `lines`.name AS line, series.name AS serie, trailers.date FROM trailers INNER JOIN trains ON trains.id = trailers.id_train INNER JOIN `lines` ON`lines`.id = trains.id_line INNER JOIN series ON series.id = trains.id_serie';
+      const sql = 'SELECT `lines`.name AS line, series.name AS serie, trains.id AS train, users.id AS CP, users.firstname, users.lastname, centers.name AS center, number AS trailer, trailers.id, `date`, processingTime FROM trailers INNER JOIN trains ON trains.id = trailers.id_train INNER JOIN `lines` ON`lines`.id = trains.id_line INNER JOIN series ON series.id = trains.id_serie INNER JOIN users ON users.id = trailers.id_user INNER JOIN centers ON centers.id = users.id_center ORDER BY id_train, trailers.number';
       const result = await connection.promise().query(sql);
       return result[0];
     } catch (error) {
