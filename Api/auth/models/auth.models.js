8f7619294f9de914fba5cc25e3ddebef314afd05
DOUENCE Gael (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DIR) <8902809s@commun.ad.sncf.fr>
2022-07-25 13:20:14 +0200

    commentaires dans auth model

--- Api/auth/models/auth.models.js
+++ Api/auth/models/auth.models.js
@@ -7,7 +7,9 @@ class AuthModels {
   async getUserById(id) {
     // Récupère un utilisateur spécifique avec son numéro de CP depuis la db
     try {
-      const sql = 'SELECT users.id, firstname, lastname, email, password, centers.name AS center , profiles.name AS profile FROM users INNER JOIN centers ON centers.id = users.id_center INNER JOIN profiles ON profiles.id = users.id_profile WHERE users.id=? ';
+      const sql = 'SELECT users.id, firstname, lastname, email, password, centers.name AS center' +
+      ', profiles.name AS profile FROM users INNER JOIN centers ON centers.id = users.id_center' + 
+      'INNER JOIN profiles ON profiles.id = users.id_profile WHERE users.id=? ';
       const result = await connection.promise().query(sql, [id]);
       return result[0];
     } catch (error) {
